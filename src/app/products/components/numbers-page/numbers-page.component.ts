45388003d3dbf73281ff39f38906cb887b0fd825
karlux <karlux.sistemas@gmail.com>
2023-08-22 20:27:56 -0300

    feat: add examples of numbers, currencies and percentages to numbersPage

--- src/app/products/components/numbers-page/numbers-page.component.ts
+++ src/app/products/components/numbers-page/numbers-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-numbers-page',
+  selector: 'products-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumbersPageComponent {
-
+  public totalSells = 2567789.5567;
+  public percentage = 0.4856;
 }
